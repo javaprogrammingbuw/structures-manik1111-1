d7fc7513e3e63116f8c5dc49e5c7df17e54c7a69
Manik Mehta <manik.vipandeep.mehta@uni-weimar.de>
2019-09-24 19:32:53 +0530

    Completed switch exercise.

--- SwitchCaseMod.java
+++ SwitchCaseMod.java
@@ -11,6 +11,7 @@ public class SwitchCaseMod {
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
+		int c;
 		
 		/*
 		 * Todo: First check if 0<b<10.If this condition does not hold, print a little
@@ -18,7 +19,51 @@ public class SwitchCaseMod {
 		 * If this conditions holds, perform a modulo operation for a%b.
 		 * Print the result as a String (e.g. "five"). You can achieve that by using a switch case
 		 * */
+		if(b>0 && b<10){
+			c=a%b;
+			switch (c){
 
+				case 0:System.out.println("zero");
+				break;
+				
+		        case 1:System.out.println("one");
+		        break;
+
+		        case 2:System.out.println("two");
+		        break;
+
+		        case 3:System.out.println("three");
+		        break;
+
+		        case 4:System.out.println("four");
+		        break;
+
+		        case 5:System.out.println("five");
+		        break;
+
+		        case 6:System.out.println("six");
+		        break;
+
+		        case 7:System.out.println("seven");
+		        break;
+
+		        case 8:System.out.println("eight");
+		        break;
+
+		        case 9:System.out.println("nine");
+		        break;
+
+		        default:System.out.println("Something went wrong");
+		    }
+
+
+
+        }else{
+        System.out.println("The input value is incorrect.");
+        }
+
+        
+      
 	}
 
 }
