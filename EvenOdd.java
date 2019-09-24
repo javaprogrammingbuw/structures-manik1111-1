bb4c8f2b9b89bfa7bd9a40763f8459f747a46325
Manik Mehta <manik.vipandeep.mehta@uni-weimar.de>
2019-09-24 19:32:09 +0530

    Completed loop exercise.

--- EvenOdd.java
+++ EvenOdd.java
@@ -6,10 +6,18 @@ public class EvenOdd {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
 		
 		//todo: The program takes a number as input.
 		//It then goes through all integer values till reaching that value and prints all the odd ones.
-		
+		for(int i=1;i<n;i++){
+			if(i%2==0){
+				i=i+1;
+				System.out.println(i);
+			}else{
+				System.out.println(i);
+			}
+		}
 	}
 	
 }
